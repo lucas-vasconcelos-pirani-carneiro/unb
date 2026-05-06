e22837be67af107a3fd44b4c1dc6b8d515c856c2
lucas-vasconcelos-pirani-carneiro <lucasvasconcelospiranicarneiro@gmail.com>
2026-05-06 00:02:30 -0300

    feat: Add test for generation keys and primes

--- seguranca-computacional/trabalhos/trabalho-02/code/main.py
+++ seguranca-computacional/trabalhos/trabalho-02/code/main.py
@@ -1,10 +1,28 @@
 import keygen
 import signature
 
+print('Geração de Chaves')
+p, q = keygen.gerarprimos()
+
+print(f'p = {p}\n q = {q}')
+
+print("p eh primo?", keygen.miller_rabin(p))
+print("q eh primo?", keygen.miller_rabin(q))
+
+chavepub, chavepriv = keygen.gerarchave(p, q)
+
+e, n = chavepub
+d, _ = chavepriv
+
+print("Chave pública: ", chavepub)
+print("Chave privada: ", chavepriv)
+
+"""
 primos = keygen.gerarprimos()
 print (primos)
 chaves = keygen.gerarchave(primos[0], primos[1])
 print("\n")
 print(chaves)
-signature.hasher("exemplo.jpeg",chaves[1][0],chaves[1][1])
 
+signature.hasher("exemplo.jpeg",chaves[1][0],chaves[1][1])
+"""
