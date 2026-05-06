9e048ab1836a9c63db22f7689569544027140904
lucas-vasconcelos-pirani-carneiro <lucasvasconcelospiranicarneiro@gmail.com>
2026-05-06 11:26:29 -0300

    feat: Add genrator and verificator of signatures

--- seguranca-computacional/trabalhos/trabalho-02/code/signature.py
+++ seguranca-computacional/trabalhos/trabalho-02/code/signature.py
@@ -1,15 +1,41 @@
-import hashlib
+from hashlib import sha3_256
 import base64
-import os
+import rsa
 
-def hasher(arquivo,d,n):
-    with open (arquivo, "rb") as f:
-        mensagem = f.read()
-    hash = hashlib.sha3_256(mensagem)
-    hashbytes = hash.digest()
-    hashint = int.from_bytes(hashbytes,byteorder='big')
-    assinatura = pow(hashint,d ,n)
-    assinaturabytes = assinatura.to_bytes((n.bit_length()+7)//8,byteorder='big')
-    assinatura64 = base64.b64encode(assinaturabytes).decode('utf-8')
-    with open (arquivo+".sig","w") as fsig:
-        fsig.write(assinatura64)
+def gerar_hash(mensagem):
+    return sha3_256(mensagem).digest()
+
+
+def assinar(chave_privada, mensagem):
+    hash_msg = gerar_hash(mensagem)
+    assinatura = rsa.rsa(chave_privada, hash_msg)
+    assinatura_b64 = base64.b64encode(assinatura).decode()
+
+    documento = (
+        "-----BEGIN SIGNED MESSAGE-----\n"
+        + mensagem.decode()
+        + "\n-----SIGNATURE-----\n"
+        + assinatura_b64
+    )
+
+    return documento
+
+def parse_documento(documento):
+    partes = documento.split("\n-----SIGNATURE-----\n")
+
+    mensagem = partes[0].replace(
+        "-----BEGIN SIGNED MESSAGE-----\n",
+        ""
+    )
+
+    assinatura_b64 = partes[1]
+    assinatura = base64.b64decode(assinatura_b64)
+    return mensagem.encode(), assinatura
+
+def verificar(chave_publica, documento):
+    mensagem, assinatura = parse_documento(documento)
+    hash_original = gerar_hash(mensagem)
+    hash_recebido = rsa.rsa(chave_publica, assinatura)
+    # Retirar zeros à esquerda
+    hash_recebido = hash_recebido[-len(hash_original):]
+    return hash_original == hash_recebido
