05dc26f1764b71971435eebf19c9570c3a87f24b
lucas-vasconcelos-pirani-carneiro <lucasvasconcelospiranicarneiro@gmail.com>
2026-05-06 00:02:01 -0300

    style: change some details

--- seguranca-computacional/trabalhos/trabalho-02/code/keygen.py
+++ seguranca-computacional/trabalhos/trabalho-02/code/keygen.py
@@ -8,36 +8,36 @@ def gerarnumero():
     return numero 
 
 def miller_rabin(n,k=40):
-    if n<2:
-        return False
-    if n%2==0:
-        return False
-    r=0
-    d=n-1
+    if n < 2: return False
+    if (n % 2 == 0): return False
+    
+    r = 0
+    d = n - 1
+    
     while d % 2 == 0:
         d //= 2
-        r+=1
+        r += 1
+    
     counter=0
     for  _ in range(k):
-        a=random.randrange(2,n-1)
-        x=pow(a,d,n)
-        if x == 1 or x==n-1:
-            continue
+        a = random.randrange(2, n - 1)
+        x = pow(a, d, n)
+        if x == 1 or x == (n - 1): continue
         composto = True
-        for _ in range (r-1):
-            x=pow(x,2,n)
-            if x==n-1:
-                composto=False
+
+        for _ in range (r - 1):
+            x = pow(x, 2, n)
+            if x == (n - 1):
+                composto = False
                 break
-        if composto:
-            return False
+        if composto: return False
+    
     return True
 
 def gerarprimo():
     while True:
         numero = gerarnumero()
-        if miller_rabin(numero):
-            return numero
+        if miller_rabin(numero): return numero
 
 def egcd(a, b):
     x0, x1 = 1, 0
@@ -58,18 +58,21 @@ def modinv(a, m):
     return x % m
 
 def gerarprimos():
-    primeiro=gerarprimo()
+    primeiro = gerarprimo()
     segundo = gerarprimo()
     return primeiro, segundo
 
-
 def gerarchave(primeiro,segundo):    
-    n = primeiro*segundo
-    totiente = (primeiro-1)*(segundo-1)
-    e=65537
-    if(totiente % e==0):
+    n = primeiro * segundo
+    totiente = (primeiro - 1) * (segundo - 1)
+    e = 65537
+    
+    if math.gcd(e, totiente) != 1:
         raise ValueError("erro em gerarchave")
-    d=modinv(e,totiente)
-    chavepub=(e,n)
-    chavepriv=(d,n)
+    
+    d = modinv(e, totiente)
+    
+    chavepub = (e,n)
+    chavepriv = (d,n)
+    
     return  chavepub,chavepriv
