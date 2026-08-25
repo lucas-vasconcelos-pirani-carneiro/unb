7dd332dd2dc4c0c49208ff8ee98bf28fd7b280d8
lucas-vasconcelos-pirani-carneiro <lucasvasconcelospiranicarneiro@gmail.com>
2026-08-25 12:58:14 -0300

    feat: Add trabalho01 oac

---
+++ organizacao-e-arquitetura-de-computadores/trabalhos/trabalho01/trabalho01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include  <stdint.h>
+#define MEM_SIZE 16384
+
+int8_t mem[MEM_SIZE]; // Memória é um array de 16KBytes
+
+int main(){
+    printf("Hello World!!\n");
+    return 0;
+}
